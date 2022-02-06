da7da805e3c334c359d6203bc3deeda69946f193
Yanislav Ivanov <frexcho998@gmail.com>
2022-02-06 13:57:30 +0200

    nQueenSolver :@

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,8 +1,4 @@
-﻿using HashSet;
-using System;
-using Trees.AVL_Tree;
-using Trees.B_Tree;
-using Trees.RedBlack_Tree;
+﻿using Excercises;
 
 namespace DataStructuresAndAlgorithms
 {
@@ -10,31 +6,7 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main(string[] args)
         {
-            var hashtset = new HashSet<string>(8, 50);
-            hashtset.Add("aa");
-            hashtset.Add("ab");
-            hashtset.Add("zz");
-            hashtset.Add("ca");
-            hashtset.Add("dr");
-            hashtset.Add("abzz");
-            hashtset.Add("zzaa");
-            hashtset.Add("caza");
-            hashtset.Add("drzaq");
-            hashtset.Add("aa2");
-            hashtset.Add("ab3");
-            hashtset.Add("zz4");
-            hashtset.Add("ca5");
-            foreach (var item in hashtset.internalArray)
-            {
-                if (item == null)
-                {
-                    Console.WriteLine("Empty slot");
-                    continue;
-                }
-                Console.WriteLine(String.Join(", ", item));
-            }
-            Console.WriteLine($"Contains aa => {hashtset.Contains("aa")}");
-            Console.WriteLine($"Contains aw => {hashtset.Contains("aw")}");
+            QueenSolver.Solve();
         }
     }
 }
---
+++ Excercises/QueenSolver.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excercises
+{
+    public static class QueenSolver
+    {
+        private static int PossibleSolutions = 0;
+        private const int Size = 8;
+        static int[,] board = new int[Size, Size];
+        static HashSet<int> Rows = new HashSet<int>();
+        static HashSet<int> Cols = new HashSet<int>();
+        public static void Solve(int row = 0)
+        {
+            if (row == Size)
+            {
+                PrintSolution();
+                return;
+            }
+            else
+            {
+                for (int col = 0; col < Size; col++)
+                {
+                    if(IsSafeToPlaceQueen(row, col))
+                    {
+                        MarkAttakedFields(row, col);
+                        Solve(row + 1);
+                        UnmarkAttakedFields(row, col);
+                        
+                    }
+                }
+            }
+        }
+
+        private static void PrintSolution()
+        {
+            int rowLength = board.GetLength(0);
+            int colLength = board.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", board[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            PossibleSolutions++;
+            Console.WriteLine(PossibleSolutions);
+            
+        }
+
+        private static void UnmarkAttakedFields(int row, int col)
+        {
+            board[row, col] = 0;
+            Rows.Remove(row);
+            Cols.Remove(col);
+        }
+
+        private static void MarkAttakedFields(int row, int col)
+        {
+            board[row, col] = 1;
+            Rows.Add(row);
+            Cols.Add(col);
+        }
+
+        private static bool IsSafeToPlaceQueen(int row, int col)
+        {
+            if (Rows.Contains(row)) return false;
+            if (Cols.Contains(col)) return false;
+            // could be done better, but prefer to not :D 
+            for (int i = 1; i < Size; i++)
+            {
+                int currentRow = row - 1;
+                int currentCol = col - 1;
+
+                if(currentRow<0 || currentRow>=Size || currentCol<0 || currentCol >= Size)
+                {
+                    break;
+                }
+                if(board[currentRow, currentCol] == 1)
+                {
+                    return false;
+                }
+            }
+            for (int i = 1; i < Size; i++)
+            {
+                int currentRow = row + 1;
+                int currentCol = col + 1;
+
+                if (currentRow < 0 || currentRow >= Size || currentCol < 0 || currentCol >= Size)
+                {
+                    break;
+                }
+                if (board[currentRow, currentCol] == 1)
+                {
+                    return false;
+                }
+            }
+            for (int i = 1; i < Size; i++)
+            {
+                int currentRow = row + 1;
+                int currentCol = col - 1;
+
+                if (currentRow < 0 || currentRow >= Size || currentCol < 0 || currentCol >= Size)
+                {
+                    break;
+                }
+                if (board[currentRow, currentCol] == 1)
+                {
+                    return false;
+                }
+            }
+            for (int i = 1; i < Size; i++)
+            {
+                int currentRow = row - 1;
+                int currentCol = col + 1;
+
+                if (currentRow < 0 || currentRow >= Size || currentCol < 0 || currentCol >= Size)
+                {
+                    break;
+                }
+                if (board[currentRow, currentCol] == 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
